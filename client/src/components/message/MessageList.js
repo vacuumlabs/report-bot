bf5af0bf3280063580880694531dc21739861df4
Samuel Hapák <samuel.hapak@gmail.com>
2019-06-15 20:58:55 +0200

    Some random cleanup
    Remove stuff, change stuff, fix stuff, whatever.

--- client/src/components/message/MessageList.js
+++ client/src/components/message/MessageList.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import MessageItem from './MessageItem'
+import {Link} from '../ui'
+import MessageContent from './MessageContent'
+import ParentPreview from './ParentPreview'
+import Replies from './Replies'
+import {formatTs} from '../../utils/helpers'
+
 import './MessageList.scss'
 
 function MessageList({customEmojis, reports, users, channels}) {
@@ -13,3 +18,46 @@ function MessageList({customEmojis, reports, users, channels}) {
 }
 
 export default MessageList
+
+
+function MessageItem({message, customEmojis, users, channels}) {
+  const author = users[message.user]
+  const firstReplyAuthor = message.replies.length >= 2 && users[message.replies[1].user]
+  const responseTo = message.response_to
+  const channelName = channels[message.channel].name
+  const dateTime = formatTs(message.ts)
+
+  return (
+    <div className="MessageItem">
+      <a className="authorPicture">
+        <img src={author.image_48} srcSet={`${author.image_72} 2x`} />
+      </a>
+      <div className="messageData">
+        <div className="messageInfo">
+          <div className="authorName">{author.real_name}</div>
+          <div className="messageDateTimeChannel">{dateTime} in {channelName}</div>
+          <div className="messagePermalink">
+            <Link to={message.permalink}>Go to message on Slack</Link>
+          </div>
+        </div>
+        { !!responseTo &&
+          <ParentPreview
+            users={users}
+            channel={message.channel}
+            customEmojis={customEmojis}
+            parentTs={responseTo}
+            parentText={message.replies[0].text}
+            threadTs={responseTo}
+          />
+        }
+        <MessageContent text={message.message} users={users} customEmojis={customEmojis} />
+        {
+          responseTo
+            ? <Replies channel={message.channel} customText={'View newer replies'} repliesCount={message.replies.length} ts={responseTo} />
+            : <Replies channel={message.channel} firstReplyAuthorPicture={firstReplyAuthor && firstReplyAuthor.image_32} repliesCount={message.replies.length} ts={message.ts} />
+        }
+        
+      </div>
+    </div>
+  )
+}
