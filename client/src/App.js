3f0b8d30ad6a3a7f8e88c8e20c85e256a4c50a3d
Tomas Fabry <tomas.fabry@vacuumlabs.com>
2019-06-24 23:14:03 +0200

    Load first tag on default page + added loadings + info if there is no tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { getTags } from './utils/serverApi'
 import LeftPanel from './components/leftPanel/LeftPanel'
 import Content from './components/content/Content'
 import './App.scss'
 
 class App extends Component {
+  state = {
+    loading: true,
+    tags: [],
+  }
+
+  async componentWillMount() {
+    const tags = await getTags()
+
+    this.setState({
+      loading: false,
+      tags,
+    })
+  }
+
   render() {
+    const { loading, tags } = this.state
+    const firstTag = tags.length ? tags[0].tag : undefined
+
     return (
       <div className="container">
         <BrowserRouter>
-          <LeftPanel />
+          <LeftPanel loading={loading} tags={tags} />
           <Switch>
             <Route exact path="/:tag" component={Content} />
+            <Route render={(props) => <Content {...props} defaultTag={firstTag} loading={loading} />} />
           </Switch>
         </BrowserRouter>
       </div>
--- client/src/components/content/Content.js
+++ client/src/components/content/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TopPanel from '../topPanel/TopPanel'
 import MessageList from '../message/MessageList'
+import { Loader } from '../ui'
 import { getReportsByTag } from '../../utils/serverApi'
 import './Content.scss'
 
@@ -8,7 +9,7 @@ class Content extends Component {
   state = {
     emoji: {},
     reports: [],
-    selectedTag: null,
+    loading: true,
   }
 
   componentWillReceiveProps(nextProps) {
@@ -20,8 +21,13 @@ class Content extends Component {
   }
 
   createState = async (props = this.props) => {
+    this.setState({
+      loading: true
+    })
+
     const { tag } = props.match.params
-    const selectedTag = decodeURI(tag)
+    let selectedTag = tag || props.defaultTag
+    selectedTag = selectedTag ? decodeURI(selectedTag) : selectedTag
 
     const {reports, users, emoji, channels} = await getReportsByTag(selectedTag)
     
@@ -31,16 +37,31 @@ class Content extends Component {
       emoji,
       channels,
       selectedTag,
+      loading: props.loading,
     })
   }
 
   render() {
-    const {emoji, reports, users, channels, selectedTag} = this.state
+    const {emoji, reports, users, channels, selectedTag, loading} = this.state
 
     return (
       <div className="Content">
-        <TopPanel selectedTag={selectedTag} customEmojis={emoji} />
-        <MessageList reports={reports} users={users} channels={channels} customEmojis={emoji} />
+        {loading && 
+          <div className="info">
+            <Loader />
+          </div>
+        }
+        {!loading && !selectedTag &&
+          <div className="info">
+            No messages.
+          </div>
+        }
+        {!loading && selectedTag &&
+          <>
+            <TopPanel selectedTag={selectedTag} customEmojis={emoji} />
+            <MessageList reports={reports} users={users} channels={channels} customEmojis={emoji} />
+          </>
+        }
       </div>
     )
   }
