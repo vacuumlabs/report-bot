e5741c37e3539a2953dc7e2659e8cc9fe97a8ae8
Samuel Hapák <samuel.hapak@gmail.com>
2019-07-22 14:26:27 +0200

    Fix #4 render all slack emojis correctly
    Most of slack emojis weren't working because they need to be translated
    to unicode properly. I've used this post as an inspiration for the fix:
    https://aaronparecki.com/2017/02/05/8/day-47-slack-emoji

--- client/src/components/ui/Emoji.js
+++ client/src/components/ui/Emoji.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import ReactEmoji from 'react-emoji-render'
+import emojiData from 'emoji-datasource/emoji.json'
+import punycode from 'punycode'
 import './Emoji.scss'
+
+function name2unicode(str) {
+  const record = emojiData.find((e) => e.short_name === str)
+  if (record == null) return str
+
+  return punycode.ucs2.encode(
+    record.unified
+      .split('-')
+      .map((p) => parseInt(p, 16))
+  )
+}
 
 function Emoji({name, customEmojis}) {
   const src = customEmojis[name] || name
@@ -8,7 +20,7 @@ function Emoji({name, customEmojis}) {
     <span role='img' className='emoji' style={{backgroundImage: `url("${src}")`}}>
       {name}
     </span>
-  ) : <ReactEmoji text={`:${src}:`} />
+  ) : name2unicode(src)
 }
 
 export default Emoji
