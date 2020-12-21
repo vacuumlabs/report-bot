50034da02ccc0546c8e5a9481f0354ae7d04f7bf
matusziak <matuszia303@gmail.com>
2020-12-21 02:43:03 +0100

    feat(portfolios): portfolios multiselect component

---
+++ client/src/components/leftPanel/PortfoliosSelect.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import MultiSelect from 'react-multi-select-component'
+
+import './PortfoliosSelect.scss'
+import settingsIcon from '../../assets/settings.svg'
+
+export function portfolioStringsToObjects(portfolioStrings) {
+	return portfolioStrings ? portfolioStrings.map((p) => ({ label: p, value: p })) : []
+}
+
+function PortfoliosSelect({value, onChange, options, placeholder, showSettings, history}) {
+	
+	const onSettingsClick = () => {
+    history.push(`/editPortfolios`)
+	}
+	
+	return (<div className="multiSelectContainer">
+		<MultiSelect
+			className="multiSelect"
+			options={options}
+			value={value}
+			onChange={onChange}
+			overrideStrings={{ "selectSomeItems": placeholder }}
+		/>
+		{showSettings && (
+			<img 
+				src={settingsIcon}
+				className="settings"
+				alt="settings"
+				onClick={onSettingsClick} 
+			/>
+		)}
+	</div>)
+}
+
+
+export default withRouter(PortfoliosSelect)
